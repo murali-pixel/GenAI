4e8bef3d74e54615bb8f367c8da62bb31d783a9f
Sushmitha Batchu <sushmitha.batchu@springml.com>
2023-06-30 19:24:48 -0230

    updated the code to generate transcript text for the given audio or video files

---
+++ config.py
@@ -0,0 +1,7 @@
+#ROOT_PATH = "/Users/sushmithabatchu/Development/GenAISolutions/summarizedText/"
+import os
+ROOT_PATH = os.getcwd()
+BUCKET_NAME = 'transcript_download'
+
+VIDEO_EXTENSIONS = ['mp4', 'ogg', 'webm', 'avi', 'mov', 'mpg']
+AUDIO_EXTENSIONS = ['mp3', 'MP3', 'm4a', 'wma', 'wav']
