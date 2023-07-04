1a41cebdfbce58a84eff77183ad74e42a20de030
Sushmitha Batchu <sushmitha.batchu@springml.com>
2023-07-04 16:32:26 -0230

    gen_ai gcp model

--- genai_test.py
+++ genai_test.py
@@ -1,5 +1,20 @@
 from vertexai.preview.language_models import TextGenerationModel
 
+meeting_transcript = """Good morning, everyone. Let's begin the meeting.",
+        "I hope everyone had a productive week.",
+        "Absolutely, I made some progress on the project we discussed last time.",
+        "That's great to hear. I'm looking forward to your update.",
+        "I've completed the initial research and compiled a list of potential solutions.",
+        "Excellent work. I appreciate your thoroughness.",
+        "Have you considered the budget implications of the proposed solutions?",
+        "Yes, I've taken the budget into account and identified cost-effective options.",
+        "I have a few suggestions regarding implementation. Can we discuss those?",
+        "Certainly, let's hear your ideas.",
+        "I believe incorporating agile methodologies would streamline the development process and improve collaboration.",
+        "I agree, agile practices have proven to be effective in similar projects.",
+        "Thank you, everyone, for your valuable inputs. Let's summarize the action items and assign responsibilities.",
+        "Meeting adjourned."""
+
 
 def interview(temperature: float = 0.2):
     """Ideation example with a Large Language Model"""
@@ -14,7 +29,8 @@ def interview(temperature: float = 0.2):
 
     model = TextGenerationModel.from_pretrained("text-bison@001")
     response = model.predict(
-        "Give me ten interview questions for the role of program manager.",
+        # prompt = "Give me ten interview questions for the role of program manager.",
+        prompt="Summarize this:" + meeting_transcript,
         **parameters,
     )
     print(f"Response from Model: {response.text}")
